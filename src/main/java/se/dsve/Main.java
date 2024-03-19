0c5c0ce36d67590da97145e51cf46f3d0d84c268
Lars Strömberg <lars@dsve.se>
2024-03-19 14:59:19 +0000

    Add Main.java

---
+++ src/main/java/se/dsve/Main.java
@@ -0,0 +1,11 @@
+package se.dsve;
+
+// https://open-meteo.com/
+// https://open-meteo.com/doc
+
+public class Main {
+    public static void main(String[] args) {
+        WeatherApp weatherApp = new WeatherApp();
+        weatherApp.run();
+    }
+}
